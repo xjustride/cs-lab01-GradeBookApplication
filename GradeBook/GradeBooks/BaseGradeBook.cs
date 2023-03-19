ba421253c81d68114b1bc71cb3591fa1140528ee
Remigiusz Krupa <115359807+xjustride@users.noreply.github.com>
2023-03-19 14:04:20 +0100

    Refactor constructor of `BaseGradeBook`
    - [ ] Add a `bool` to the `BaseGradeBook` constructor
    			- This should be of type `bool`.
    			- This should be the second parameter.
    		- [ ] Set `IsWeight` in the `BaseGradeBook` constructor
    			- [ ] Set the `IsWeighted` property using the `bool` parameter
    		- [ ] Add a `bool` to the `StandardGradeBook` constructor
    			- This should be of type `bool`.
    			- This should be the second parameter.
    			- This will require the bool to be added to the call to the base constructor.
    		- [ ] Add a `bool` to the `RankedGradeBook` constructor
    			- This should be of type `bool`.
    			- This should be the second parameter.
    			- This will require the bool to be added to the call to the base constructor.
    		- [ ] Update `StartingUserInterface.CreateCommand` condition
    			- Change the condition checking if `parts` is not equal to 3 to be is not equal to 4.

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -18,9 +18,10 @@ namespace GradeBook.GradeBooks
         public bool IsWeighted { get; set; }
 
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
-            Name = name;
+            IsWeighted = isWeighted;    
+			Name = name;
             Students = new List<Student>();
         }
 
--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,7 +7,7 @@ namespace GradeBook.GradeBooks
 {
 	public class RankedGradeBook : BaseGradeBook
 	{
-		public RankedGradeBook(string name, bool isWeighted) : base(name) 
+		public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted) 
 		{
 			Type = Enums.GradeBookType.Ranked;
 		}
